e082d179552ec3165a047a8590680b5c1565f717
RodrigoInst1 <rcatuntas@fcpn.edu.bo>
2025-08-26 22:10:17 -0400

    Progra 2

---
+++ Programacion 2/Ejer1_EcuacionLineal/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Actualizacion prueba 2");
+        System.out.println("Actualizacion prueba 3");
+    }
+}
